fbf96745c8a86b722004e320f31ca9cc663f25c7
SammyDeAgent <rimamotostheking@gmail.com>
2021-05-22 15:55:19 +0800

    Module reinitialize

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-const http = require('http');
-const fs = require('fs');
-const hostname = 'localhost';
+// const http = require('http');
+// const fs = require('fs');
+// const hostname = 'localhost';
 
-const port = 4200;
-var html;
+// const port = 4200;
+// var html;
 
-const server = http.createServer((req, res) => {
-	if(req.url === "/"){
-		res.statusCode = 200;
-  		res.setHeader('Content-Type', 'text/html');
-  		html = fs.readFileSync('www/home.html');
-  		res.write(html);
-  		res.end();
-	}else {
-		res.statusCode = 404;
-		res.end("<html><body><title>Osmium 404</title><h1>Error 404: Osmium Unknown Directive</h1></body></html>");
-	}
+// const server = http.createServer((req, res) => {
+// 	if(req.url === "/"){
+// 		res.statusCode = 200;
+//   		res.setHeader('Content-Type', 'text/html');
+//   		html = fs.readFileSync('www/home.html');
+//   		res.write(html);
+//   		res.end();
+// 	}else {
+// 		res.statusCode = 404;
+// 		res.end("<html><body><title>Osmium 404</title><h1>Error 404: Osmium Unknown Directive</h1></body></html>");
+// 	}
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 4200;
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+	res.sendFile(path.join(__dirname,'www/home.html'));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port);
