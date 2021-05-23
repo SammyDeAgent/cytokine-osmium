3a9b8fed7e3ae2c0f8c000bf5408cc0609252aa4
SammyDeAgent <rimamotostheking@gmail.com>
2021-05-23 13:13:39 +0000

    Protocol redirect fix

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ const port = process.env.PORT || 4200;
 
 app.use(express.static('public'));
 
-// Redirect http to https
-app.use(function(req,res){
-	if(!req.secure){
-		res.redirect("https://"+req.headers.host+req.url);
-	}
-});
-
 app.get('/', function(req,res){
 	res.sendFile(path.join(__dirname,'www/home.html'));
 });
