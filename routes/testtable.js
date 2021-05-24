a92d3fd397ff53dfda3a0bfca30defa0186e7ef8
SammyDeAgent <rimamotostheking@gmail.com>
2021-05-25 03:30:50 +0800

    Hot fix

--- routes/testtable.js
+++ routes/testtable.js
@@ -1,7 +1,7 @@
 exports.list = function(req, res){
 
   req.getConnection(function(err,connection){   
-      var query = connection.query('SELECT * FROM test',function(err,rows){
+      connection.query('SELECT * FROM test',function(err,rows){
         if(err)
           console.log("Error Selecting : %s ",err );
 
