cbfd08d0c98bacdbb60a0a716527aa9010dad2fd
SammyDeAgent <rimamotostheking@gmail.com>
2021-05-24 08:55:49 +0000

    Implementing SQL support

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const path = require('path');
 
+const mysql = require('mysql');
+const con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "Sql_osmium576",
+	database: "osmium"
+});
+con.connect(function(err){
+	if(err) console.log("Oops...");
+	console.log("Connection established.");
+});
+
+// var testtable = require('./routes/testtable');
+
 const app = express();
 const port = process.env.PORT || 4200;
 
 app.use(express.static('public'));
+// app.set('views', __dirname + '/views');
+
+// app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
 	res.sendFile(path.join(__dirname,'www/home.html'));
@@ -14,4 +31,10 @@ app.get('/default', function(req,res){
 	res.sendFile(path.join(__dirname,'www/default.html'));
 });
 
+// app.get('/query', testtable.list);
+
+// app.use(app.router);
+
 app.listen(port);
+
+module.exports = app;
