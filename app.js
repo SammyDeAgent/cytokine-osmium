e0212f678fed843a56ac3bb97f6e72cd1b739b44
SammyDeAgent <rimamotostheking@gmail.com>
2021-05-25 03:24:00 +0800

    Updated SQL support

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
+// Module Imports
 const express = require('express');
 const path = require('path');
 
+// App initialization
+const app = express();
+const port = process.env.PORT || 4200;
+
+// SQL Connection
 const mysql = require('mysql');
-const con = mysql.createConnection({
+const connection = require('express-myconnection'),
+dbOptions = {
 	host: "localhost",
 	user: "root",
 	password: "Sql_osmium576",
 	database: "osmium"
-});
-con.connect(function(err){
-	if(err) console.log("Oops...");
-	console.log("Connection established.");
-});
+};
+app.use(connection(mysql, dbOptions, 'request'));
 
-// var testtable = require('./routes/testtable');
-
-const app = express();
-const port = process.env.PORT || 4200;
+// Routes
+var testtable = require('./routes/testtable');
 
 app.use(express.static('public'));
-// app.set('views', __dirname + '/views');
 
-// app.set('view engine', 'ejs');
+// View engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
+// Servers
 app.get('/', function(req,res){
 	res.sendFile(path.join(__dirname,'www/home.html'));
 });
@@ -31,9 +35,7 @@ app.get('/default', function(req,res){
 	res.sendFile(path.join(__dirname,'www/default.html'));
 });
 
-// app.get('/query', testtable.list);
-
-// app.use(app.router);
+app.get('/query', testtable.list);
 
 app.listen(port);
 
